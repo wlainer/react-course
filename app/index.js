61c69c9fb57f7e20977f40b3ed84699f4b8813ab
Wlainer Ribeiro <wlainer@gmail.com>
2016-03-15 17:09:55 -0300

    updating

--- app/index.js
+++ app/index.js
@@ -1,12 +1,50 @@
+var USER_DATA = {
+  name: "Tyler Mcginnis",
+  username: "tylermcginnis",
+  image: "https://avatars0.githubusercontent.com/u/2933430?v3&s=460"
+}
+
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height:100, width:100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function() {
     return (
-      <div> Hello World Js Program </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
-ReactDom.render(<HelloWorld />, document.getElementById('app'));
+var ProfileName = React.createClass({
+  render: function() {
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+
+ReactDom.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);
